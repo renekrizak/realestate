bc3ff60f9e2a967d0f0da29ce588bcea458bdda4
Rene Krizak <rene.krizak@gmail.com>
2023-08-10 13:46:03 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Listings from "./pages/Listings";
+import Services from "./pages/Services";
+import Blogs from "./pages/Blogs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/Blogs" element={<Blogs />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Listings.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Listings = () => {
+  return <></>;
+};
+
+export default Listings;
